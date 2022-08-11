574e7e5d4a3be8352c7ab694134e204164fe5467
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2022-08-10 23:52:50 -0300

    tamanho de um ponteiro

---
+++ aula_1/tamponteiro_4.c
@@ -0,0 +1,17 @@
+//independere do tipo de ponteiro, não há diferenç a no tamannho de um ponteiro
+//todos ponteiros guardam endereço de memória
+//endereço de memória possuem o mesmo tamanho
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+
+    int *p1;
+    char *p2;
+    float *p3;
+
+    printf("tamanho de p1: %d\n", (int)sizeof(p1));
+    printf("tamanho de p2: %d\n", (int)sizeof(p2));
+    printf("tamanho de p3: %d\n", (int)sizeof(p3));
+};
