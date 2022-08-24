ed7c841a718c7898763ed3026e67de5d4bcc1f56
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2022-08-24 20:21:43 -0300

    terminar

--- aula_9/exe_1.c
+++ aula_9/exe_1.c
@@ -20,3 +20,32 @@ typedef struct {
     No *topo;
     int tam;
 }Pilha;
+
+
+void criar_pilha(Pilha *p){
+    p -> topo = NULL;
+    p -> tam = 0;
+}
+
+ void empilhar(Pilha *p){
+    No *novo = malloc(sizeof(No));
+
+    if(novo){
+        novo -> p = ler_pessoa();
+        novo -> proximo = p->topo;
+        p -> topo = novo;
+        p -> tam++;
+    }else   
+        printf("\nErro ao alocar memória...\n");
+ }
+
+No *desempilhar(Pilha *p){
+    if(p->topo){
+        No *remover = p ->topo;
+        p-> topo= remover->proximo;
+        p->tam--;
+        return remover;
+    }else
+        printf("\nPilha vazia!\n");
+        return NULL;
+}
