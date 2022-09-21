1b3c3b55eb6de45b626944457e38b55d5f2b6bf4
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2022-09-21 20:51:03 -0300

    pilha com prototipação #ifndef PILHA_H

--- Ponteiros/ponteiros_vetores.c
+++ Ponteiros/ponteiros_vetores.c
@@ -6,11 +6,21 @@ void imprimirvet(int *v,int tam){
     for(i = 0; tam < v; i++){
         printf("%d ", *(v + i));
     }
+    printf("\n");
+}
+
+void preencervetorInt( int *v,int tam){
+    int i;
+    for (i = 0; i < tam; i++){
+    printf("digit qualquer valor\n");
+    scanf("%d", v + i);
+    } 
 }
 int main (){
     
     int i, vet[10] = {1,2,3,4,5,6,7,8,9,10};
 
-    imprimirvet(vet,10);
+    preencervetorInt(vet,10);
+    imprimirvet(vet,10);  
 
 }
